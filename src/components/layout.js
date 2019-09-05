4c8ba2a42a000184e92fc5a8dcf5d745ab02ffc9
Surendran <surendran@hasura.io>
2019-09-05 16:15:38 +0530

    style scss update

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header/>
       <div
         style={{
           margin: `0 auto`,
