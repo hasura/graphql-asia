d76fea73183ffb7fd0f215e065adfef21411a7f1
surendran82 <surendran@hasura.io>
2019-09-21 16:47:18 +0530

    update ticket page

---
+++ src/pages/tickets.js
@@ -0,0 +1,18 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import '../components/styles.scss'
+import TicketsTopBanner from '../components/TicketsTopBanner'
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Tickets" />
+      <TicketsTopBanner/>
+      <div className={'ticketsWrapper'}>
+        <iframe title="tickets" src="https://www.townscript.com/widget/graphql-asia--2020" frameBorder="0" height="770" width="100%"></iframe>
+      </div>
+  </Layout>
+)
+
+export default IndexPage
