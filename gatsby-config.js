b4794747f2aedc630f4bf9907b9f1ef8ad690ccf
Surendran <surendran@hasura.io>
2019-09-24 14:43:56 +0530

    google analytics updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+        trackingId: `UA-148609270-1`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -27,6 +28,12 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-148609270-1`,
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
