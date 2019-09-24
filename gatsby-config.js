d66254573dff180ebd65f40587f2f6b7e358486b
Surendran <surendran@hasura.io>
2019-09-24 12:50:09 +0530

    meta description update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `GraphQL Asia 2020`,
-    description: `GraphQL Asia is the largest gathering of the GraphQL community in Asia! With over 500 people expected at the 2020 edition, you’ll meet the creators of leading GraphQL tooling, talk to the experts implementing GraphQL in their companies as well as practitioners and enthusiasts from Asia and the rest of the world.`,
-    author: `@gatsbyjs`,
+    description: "GraphQL Asia is the largest gathering of the GraphQL community in Asia! With over 500 people expected at the 2020 edition, you’ll meet the creators of leading GraphQL tooling, talk to the experts implementing GraphQL in their companies as well as practitioners and enthusiasts from Asia and the rest of the world.",
+    author: `@graphqlasia`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
