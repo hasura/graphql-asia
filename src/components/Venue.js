06392f8c4b53ea0af064506a3c40710dbb1dd8c9
surendran82 <surendran@hasura.io>
2019-12-24 21:08:02 +0530

    schedule section update (#21)
    * Schedule and venue section update
    
    * Schedule and venue section update
    
    * Schedule and venue section update
    
    * Venue section update
    
    * venue section image align fix

--- src/components/Venue.js
+++ src/components/Venue.js
@@ -3,17 +3,20 @@ import './styles.scss';
 const leelaPalace = require('../images/leela-palace.png');
 const Venue = () => (
   <div id="venue" className={'venueWrapper sectionWrapper watermarkWrapper lightGrayBgColor positionTop'}>
-    <div className={'container noPadd displayEnd'}>
+    <div className={'container noPadd'}>
+      <div className={'sectionHeader'}>
+        VENUE
+      </div>
+      <div className={'pinkSeperateLine'}></div>
       <div className={'col-md-6 col-sm-7 col-xs-12 noPadd'}>
-        <div className={'sectionHeader'}>
-          VENUE
-        </div>
-        <div className={'pinkSeperateLine'}></div>
         <div className={'pageDescription'}>
-          The Leela Palace Bengaluru is one of the best 5 star hotels in Bangalore, India stands majestically amidst seven acres of lush gardens and a sparkling lagoon.
+          The conference will take place at The Leela Palace, which is one of the best 5 star hotels in Bengaluru, India and stands majestically amidst seven acres of lush gardens and a sparkling lagoon.
+        </div>
+        <div className={'pageDescription'}>
+          To avail special conference rates for staying at the hotel, please send an email to <a href="mailto:saikat.saha@theleela.com">saikat.saha@theleela.com</a> and <a href="mailto:namrata.gupta@theleela.com">namrata.gupta@theleela.com</a> along with a copy of your ticket.
         </div>
         <div className={'pageDescription removePaddBottom'}>
-          To avail special conference rates for staying at the hotel, please send an email to <a href="mailto:saikat.saha@theleela.com">saikat.saha@theleela.com</a> and <a href="mailto:namrata.gupta@theleela.com">namrata.gupta@theleela.com</a> along with a copy of your ticket.
+          The workshops will be hosted at the <a href="https://theterrace.hasura.io/" target="_blank" rel="noopener noreferrer">Terrace @ Hasura</a>.
         </div>
       </div>
       <div className={'col-md-6 col-sm-5 col-xs-12'}>
