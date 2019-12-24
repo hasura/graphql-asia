f72689880ee5594515f4e679724eb7871c1c6a85
Surendran <surendran@hasura.io>
2019-12-24 20:59:28 +0530

    venue section image align fix

--- src/components/Venue.js
+++ src/components/Venue.js
@@ -3,12 +3,12 @@ import './styles.scss';
 const leelaPalace = require('../images/leela-palace.png');
 const Venue = () => (
   <div id="venue" className={'venueWrapper sectionWrapper watermarkWrapper lightGrayBgColor positionTop'}>
-    <div className={'container noPadd displayEnd'}>
+    <div className={'container noPadd'}>
+      <div className={'sectionHeader'}>
+        VENUE
+      </div>
+      <div className={'pinkSeperateLine'}></div>
       <div className={'col-md-6 col-sm-7 col-xs-12 noPadd'}>
-        <div className={'sectionHeader'}>
-          VENUE
-        </div>
-        <div className={'pinkSeperateLine'}></div>
         <div className={'pageDescription'}>
           The conference will take place at The Leela Palace, which is one of the best 5 star hotels in Bengaluru, India and stands majestically amidst seven acres of lush gardens and a sparkling lagoon.
         </div>
