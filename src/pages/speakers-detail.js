c84ac56b6166a1f3c5f8d4499203fba53d3c3909
Surendran <surendran@hasura.io>
2020-01-11 18:19:27 +0530

    Speakers detail page WIP

--- src/pages/speakers-detail.js
+++ src/pages/speakers-detail.js
@@ -2,6 +2,7 @@ import React from "react"
 // import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Contact from '../components/Contact'
 import SEO from "../components/seo"
 import '../components/styles.scss'
 import DetailsTopBanner from '../components/DetailsTopBanner'
@@ -10,6 +11,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Speakers Details | GraphQL Asia 2020" />
       <DetailsTopBanner/>
+      <Contact/>
   </Layout>
 )
 
