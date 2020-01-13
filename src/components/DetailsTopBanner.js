eb19708087a5c42c27f559ac9cd014509317549f
Surendran <surendran@hasura.io>
2020-01-13 19:13:16 +0530

    Speakers detail page WIP

--- src/components/DetailsTopBanner.js
+++ src/components/DetailsTopBanner.js
@@ -1,33 +1,21 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 import './styles.scss'
 // const graphqlAsia = require('../images/graphql_Asia2020_heading.svg');
 // const megaphone = require('../images/megaphone.svg');
-const vishakhaSingh = require('../images/Vishakha-Singh.png');
+import { speakerState } from './AllState.js';
+
 const twitter = require('../images/twitter-pink.svg');
 const github = require('../images/github-pink.svg');
+const linkedIn = require('../images/linkedin-pink.svg');
 const arrowPink = require('../images/arrow-pink.svg');
-const speakerState = {
-  'vishakha-singh': {
-    'name': 'Vishakha Singh',
-    'designation': 'SOFTWARE ENGINEER',
-    'org': 'PAYPAL',
-    'img': { path: vishakhaSingh, alt: 'Vishakha Singh'},
-    'title': 'How we improved PayPal checkout conversions using GraphQL',
-    'description': 'At PayPal, we are rebuilding our checkout experience from the ground up. The most important and logical problem we are solving is to make the experience super fast. Come hear our story on how we leveraged GraphQL server to smartly and efficiently present varied regional payment experiences to people all around the globe.',
-    'about': 'Vishakha is a software engineer at PayPal where she works to make the checkout experience faster and better. She completed her Masters from Georgia Tech specializing in Computer Networks only to later realize her passion for Javascript and UI engineering. She also loves spending time meditating, baking and experimenting with makeup!'
-  }
-};
+
 const DetailsTopBanner = (props) => {
   const speakerUrl = props.speakerUrl;
-  console.log(speakerUrl);
-  console.log(speakerState[speakerUrl]);
-  /*
-  if(!speakerState.speakerUrl) {
+  const currentSpeaker = speakerState[speakerUrl];
+  if(!currentSpeaker) {
     window.location.href = "/404";
   }
-  */
-  const currentSpeaker = speakerState[speakerUrl];
   return (
     <div>
       <div className='detailsTopBannerWrapper detailsBgImage'>
@@ -35,7 +23,7 @@ const DetailsTopBanner = (props) => {
           <div className='detailsWrapper'>
             <div className='col-md-4 col-sm-4 col-xs-12 noPadd'>
               <div className='profileImg'>
-                <img src={currentSpeaker.img.path} alt={currentSpeaker.img.alt}/>
+                <img src={currentSpeaker.img} alt={currentSpeaker.name}/>
               </div>
             </div>
             <div className='col-md-8 col-sm-8 col-xs-12 noPadd'>
@@ -50,12 +38,27 @@ const DetailsTopBanner = (props) => {
                   {currentSpeaker.org}
                 </div>
                 <div className='socialWrapper'>
-                  <div className='socialIcon'>
-                    <a href={'https://github.com/robzhu'} target="_blank" rel="noopener noreferrer"><img src={github} alt={'github'} /></a>
-                  </div>
-                  <div className='socialIcon'>
-                    <a href={'https://twitter.com/rbzhu/'} target="_blank" rel="noopener noreferrer"><img src={twitter} alt={'Twitter'} /></a>
-                  </div>
+                  {
+                    (currentSpeaker.githubLink) ? (
+                      <div className='socialIcon'>
+                        <a href={currentSpeaker.githubLink} target="_blank" rel="noopener noreferrer"><img src={github} alt={'Github'} /></a>
+                      </div>
+                    ) : null
+                  }
+                  {
+                    (currentSpeaker.twitterLink) ? (
+                      <div className='socialIcon'>
+                        <a href={currentSpeaker.twitterLink} target="_blank" rel="noopener noreferrer"><img src={twitter} alt={'Twitter'} /></a>
+                      </div>
+                    ) : null
+                  }
+                  {
+                    (currentSpeaker.linkedInLink) ? (
+                      <div className='socialIcon'>
+                        <a href={currentSpeaker.linkedInLink} target="_blank" rel="noopener noreferrer"><img src={linkedIn} alt={'LinkedIn'} /></a>
+                      </div>
+                    ) : null
+                  }
                 </div>
               </div>
             </div>
@@ -64,33 +67,76 @@ const DetailsTopBanner = (props) => {
       </div>
       <div className='sectionWrapperSmall'>
         <div className={'container noPadd'}>
-          <div className='aboutSpeakerWrapper'>
-            <div className='col-md-4 col-sm-4 col-xs-12 noPadd'>
-              <div className='title titleTop'>
-                TALK DETAILS
+          <div className={'clearBoth'}>
+          {
+            (currentSpeaker.title) ? (
+              <div className='aboutSpeakerWrapper'>
+                <div className='col-md-4 col-sm-4 col-xs-12 noPadd'>
+                  <div className='title titleTop'>
+                    Talk Title
+                  </div>
+                </div>
+                <div className='col-md-8 col-sm-8 col-xs-12 noPadd'>
+                  <div className='speakersTitle'>
+                    {currentSpeaker.title}
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className='col-md-8 col-sm-8 col-xs-12 noPadd'>
-              <div className='speakersTitle'>
-                {currentSpeaker.title}
+            ) : null
+          }
+          {
+            (currentSpeaker.abstract) ? (
+              <div className='aboutSpeakerWrapper'>
+                <div className='col-md-4 col-sm-4 col-xs-12 noPadd'>
+                  <div className='title titleTop'>
+                    Talk Abstract
+                  </div>
+                </div>
+                <div className='col-md-8 col-sm-8 col-xs-12 noPadd'>
+                  <div className='pageDescription'>
+                    {currentSpeaker.abstract}
+                  </div>
+                </div>
               </div>
-              <div className='pageDescription'>
-                {currentSpeaker.description}
+            ): null
+          }
+          {
+            (currentSpeaker.description) ? (
+              <div className='aboutSpeakerWrapper'>
+                <div className='col-md-4 col-sm-4 col-xs-12 noPadd'>
+                  <div className='title titleTop'>
+                    Talk Description
+                  </div>
+                </div>
+                <div className='col-md-8 col-sm-8 col-xs-12 noPadd'>
+                  <div className='pageDescription'>
+                    {currentSpeaker.description}
+                  </div>
+                </div>
               </div>
-            </div>
+            ) : null
+          }
+          {
+            (currentSpeaker.about) ? (
+              <div className='aboutSpeakerWrapper'>
+                <div className='col-md-4 col-sm-4 col-xs-12 noPadd'>
+                  <div className='title'>
+                    ABOUT {currentSpeaker.name}
+                  </div>
+                </div>
+                <div className='col-md-8 col-sm-8 col-xs-12 noPadd'>
+                  <div className='pageDescription'>
+                    {currentSpeaker.about}
+                  </div>
+                </div>
+              </div>
+            ) : null
+          }
           </div>
-          <div className='aboutSpeakerWrapper'>
-            <div className='col-md-4 col-sm-4 col-xs-12 noPadd'>
-              <div className='title'>
-                ABOUT {currentSpeaker.name}
-              </div>
-            </div>
-            <div className='col-md-8 col-sm-8 col-xs-12 noPadd'>
-              <div className='pageDescription'>
-                {currentSpeaker.about}
-              </div>
-              <div className='pinkLink'>
-                <a href="/#speakers">BACK TO SPEAKERS<img src={arrowPink} alt={'Arrow'} /></a>
+          <div className={'clearBoth aboutSpeakerWrapper'}>
+            <div className={'col-md-offset-4 col-sm-offset-4'}>
+              <div className='pinkLink '>
+                <Link to="/#speakers">BACK TO SPEAKERS<img src={arrowPink} alt={'Arrow'} /></Link>
               </div>
             </div>
           </div>
