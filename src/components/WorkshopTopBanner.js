36445048c463a17bf72bf87636fd31b40d370555
Surendran <surendran@hasura.io>
2020-01-31 18:44:47 +0530

    contact form update in workshop details page

--- src/components/WorkshopTopBanner.js
+++ src/components/WorkshopTopBanner.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import './styles.scss'
 import Layout from './layout';
-import Contact from "./Contact"
+import Contact from './Contact'
 // const graphqlAsia = require('../images/graphql_Asia2020_heading.svg');
 // const megaphone = require('../images/megaphone.svg');
 import { workshopState } from './AllState.js';
