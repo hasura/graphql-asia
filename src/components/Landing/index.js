74cccd2f0aedcfb30611aa530eb4f0ecfb87d937
lastnamearya <arya.jigyasu6815@gmail.com>
2020-11-19 12:01:27 +0530

    add landing footer section

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,6 +3,35 @@ import React from 'react';
 import graphqlAsiaImg from '../../images/graphqlAsia.png';
 import '../styles/landing.scss';
 
+const ButtonSection = () => (
+  <div className="flex justify-content-center mt-8">
+    <button className="btn-primary mr-24">
+      Signup to know About early-bird tickets<span className="right-arrow">→</span>
+    </button>
+    <button className="btn-secondary">Submit your CFP</button>
+  </div>
+);
+
+const FooterSection = () => (
+  <div className="flex align-items-center justify-content-center mt-8">
+    <div className="landing-footer-nav">
+      <a href="https://graphql.asia/2019" target="_blank" rel="noopener noreferrer">
+        GraphQL Asia 2019<span className="right-arrow">→</span>
+      </a>
+    </div>
+    <div className="landing-footer-nav">
+      <a href="https://graphql.asia/2020" target="_blank" rel="noopener noreferrer">
+        GraphQL Asia 2020<span className="right-arrow">→</span>
+      </a>
+    </div>
+    <div className="landing-footer-nav" id="graphql-asia-link">
+      <a href="https://twitter.com/graphqlasia" target="_blank" rel="noopener noreferrer">
+        @graphqlasia
+        </a>
+    </div>
+  </div>
+);
+
 export const Landing = () => (
   <div className="landing-div">
     <div className="flex flex-direction-column align-items-center">
@@ -10,14 +39,8 @@ export const Landing = () => (
       <div className="col-md-8 col-sm-8 col-xs-12">
         <h1 id="main-heading">GraphQL Asia 2021 is brewing...</h1>
         <p id="heading-desc">Online Conference | Feb 2021 </p>
-        <div className="landing-cta-div flex justify-content-center">
-          <button className="primary-cta mr-24">
-            Signup to know About early-bird tickets<span id="right-arrow">→</span>
-          </button>
-          <buton className="secondary-cta">
-            Submit your CFP
-          </buton>
-        </div>
+        <ButtonSection />
+        <FooterSection />
       </div>
     </div>
   </div>
