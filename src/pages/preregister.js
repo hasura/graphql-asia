5f80953fc529d0f57e26388c8aa637dabe7a0a58
lastnamearya <arya.jigyasu6815@gmail.com>
2020-11-19 20:15:09 +0530

    update: thankyou message

--- src/pages/preregister.js
+++ src/pages/preregister.js
@@ -20,16 +20,12 @@ const IndexPage = ({ location }) => {
     }
   }, [location.search]);
 
-  if (isAliId) {
-    return <h1 id="marketo-form-id">Thank you for signing up!</h1>
-  }
-
   return (
     <div className="landing-div">
       <div className="form-wrapper-div">
         <img src={graphqlAsiaImg} alt="graphql-asia-2021" id="graphql-asia-logo" />
         <p id="form-desc">Online Conference | Feb 2021 </p>
-        <MarketoForm formId="1068" marketoHost={marketoHost} id="631-HMN-492" styleClass="marketoFormWrapper webinarMarAuto" />
+        {isAliId ? <h1 style={{ color: 'white' }}>Thank you for signing up!</h1> : <MarketoForm formId="1068" marketoHost={marketoHost} id="631-HMN-492" styleClass="marketoFormWrapper webinarMarAuto" />}
         <div className="policy-text mt-12">
           &copy; 2020 Hasura Inc. All rights reserved | <a href="https://hasura.io/legal/hasura-privacy-policy/" target="_blank" rel="noopener noreferrer">Privacy policy</a>
         </div>
