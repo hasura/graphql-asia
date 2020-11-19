117b6bc360d0ab083450591ca72f2676231301cc
lastnamearya <arya.jigyasu6815@gmail.com>
2020-11-19 21:03:53 +0530

    fix: social share image

--- 2020/src/components/DetailsTopBanner.js
+++ 2020/src/components/DetailsTopBanner.js
@@ -82,56 +82,56 @@ const DetailsTopBanner = (props) => {
               })}
             </div>
           ) : (
-            <div className="detailsWrapper">
-              <div className="col-md-4 col-sm-4 col-xs-12 noPadd">
-                <div className="profileImg">
-                  <img src={currentSpeaker.img} alt={currentSpeaker.name} />
+              <div className="detailsWrapper">
+                <div className="col-md-4 col-sm-4 col-xs-12 noPadd">
+                  <div className="profileImg">
+                    <img src={currentSpeaker.img} alt={currentSpeaker.name} />
+                  </div>
                 </div>
-              </div>
-              <div className="col-md-8 col-sm-8 col-xs-12 noPadd">
-                <div className="profileWrapper">
-                  <div className="profileName">{currentSpeaker.name}</div>
-                  <div className="profileDesignation">{currentSpeaker.designation}</div>
-                  <div className="profileOrganization">{currentSpeaker.org}</div>
-                  <div className="socialWrapper">
-                    {currentSpeaker.githubLink ? (
-                      <div className="socialIcon">
-                        <a
-                          href={currentSpeaker.githubLink}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <img src={github} alt={'Github'} />
-                        </a>
-                      </div>
-                    ) : null}
-                    {currentSpeaker.twitterLink ? (
-                      <div className="socialIcon">
-                        <a
-                          href={currentSpeaker.twitterLink}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <img src={twitter} alt={'Twitter'} />
-                        </a>
-                      </div>
-                    ) : null}
-                    {currentSpeaker.linkedInLink ? (
-                      <div className="socialIcon">
-                        <a
-                          href={currentSpeaker.linkedInLink}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <img src={linkedIn} alt={'LinkedIn'} />
-                        </a>
-                      </div>
-                    ) : null}
+                <div className="col-md-8 col-sm-8 col-xs-12 noPadd">
+                  <div className="profileWrapper">
+                    <div className="profileName">{currentSpeaker.name}</div>
+                    <div className="profileDesignation">{currentSpeaker.designation}</div>
+                    <div className="profileOrganization">{currentSpeaker.org}</div>
+                    <div className="socialWrapper">
+                      {currentSpeaker.githubLink ? (
+                        <div className="socialIcon">
+                          <a
+                            href={currentSpeaker.githubLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <img src={github} alt={'Github'} />
+                          </a>
+                        </div>
+                      ) : null}
+                      {currentSpeaker.twitterLink ? (
+                        <div className="socialIcon">
+                          <a
+                            href={currentSpeaker.twitterLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <img src={twitter} alt={'Twitter'} />
+                          </a>
+                        </div>
+                      ) : null}
+                      {currentSpeaker.linkedInLink ? (
+                        <div className="socialIcon">
+                          <a
+                            href={currentSpeaker.linkedInLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <img src={linkedIn} alt={'LinkedIn'} />
+                          </a>
+                        </div>
+                      ) : null}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
         </div>
       </div>
       <div className="sectionWrapperSmall">
@@ -183,19 +183,19 @@ const DetailsTopBanner = (props) => {
                 })}
               </div>
             ) : (
-              <div>
-                {currentSpeaker.about ? (
-                  <div className="aboutSpeakerWrapper">
-                    <div className="col-md-4 col-sm-4 col-xs-12 noPadd">
-                      <div className="title">ABOUT {currentSpeaker.name}</div>
+                <div>
+                  {currentSpeaker.about ? (
+                    <div className="aboutSpeakerWrapper">
+                      <div className="col-md-4 col-sm-4 col-xs-12 noPadd">
+                        <div className="title">ABOUT {currentSpeaker.name}</div>
+                      </div>
+                      <div className="col-md-8 col-sm-8 col-xs-12 noPadd">
+                        <div className="pageDescription">{currentSpeaker.about}</div>
+                      </div>
                     </div>
-                    <div className="col-md-8 col-sm-8 col-xs-12 noPadd">
-                      <div className="pageDescription">{currentSpeaker.about}</div>
-                    </div>
-                  </div>
-                ) : null}
-              </div>
-            )}
+                  ) : null}
+                </div>
+              )}
           </div>
           <div className={'clearBoth aboutSpeakerWrapper'}>
             <div className={'col-md-offset-4 col-sm-offset-4'}>
