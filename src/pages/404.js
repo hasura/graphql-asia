f38677279aeda12064b40ad4c933f446e1f37734
surendran <surendran@hasura.io>
2020-12-23 19:13:33 +0530

    404 page updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,13 +6,9 @@ import '../components/Common/Styles.scss';
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <div className="notFoundBgImage">
-      <div className="notFoundWrapper">
-        <div className="container">
-          <h1>NOT FOUND</h1>
-          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-        </div>
-      </div>
+    <div className="notFoundWrapper">
+      <div className='mainTitle'>NOT FOUND</div>
+      <div className='articleDesc'>You just hit a route that doesn&#39;t exist... the sadness.</div>
     </div>
   </Layout>
 );
