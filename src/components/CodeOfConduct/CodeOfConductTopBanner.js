ca5dd9cced17fb021179ca9215c352bd260bbe8c
surendran <surendran@hasura.io>
2020-12-24 10:29:02 +0530

    fixes for responsive mobile version

--- src/components/CodeOfConduct/CodeOfConductTopBanner.js
+++ src/components/CodeOfConduct/CodeOfConductTopBanner.js
@@ -14,13 +14,13 @@ const CodeOfConductTopBanner = props => {
         All attendees, speakers, sponsors and volunteers at Enterprise GraphQL Conf 2020 are required to agree with the following code of conduct. Organisers will enforce this code throughout the event. We expect cooperation from all participants to help ensure a safe environment for everybody.<br/><br/>
         In case you have identified a breach of our CoC, please send an email addressed to <span className='redColor'>Rajoshi Ghosh</span> with the subject <span className='redColor'>Reporting CoC Violation for GraphQL Asia Conference 2021</span> immediately to <a href='mailto:events@hasura.io'>events@hasura.io</a>
       </div>
-      <div className='articleTitle'>
+      <div className='articleSubTitle'>
         The Short Version
       </div>
       <div className='articleDesc'>
         Our conference is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion (or lack thereof), or technology choices. We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any conference venue, including talks, workshops, parties, Twitter and other online media. Conference participants violating these rules may be expelled from the conference without a refund at the discretion of the conference organisers.
       </div>
-      <div className='articleTitle'>
+      <div className='articleSubTitle'>
         The Long Version
       </div>
       <div className='articleDesc removePaddBottom'>
