c287ef1790b6ea19e397e16ef0ccc1fbea07495f
surendran <surendran@hasura.io>
2021-01-13 18:08:20 +0530

    404 page props updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import { SEO } from '../components/seo';
 import '../components/Common/Styles.scss';
-const NotFoundPage = () => (
+const NotFoundPage = props => (
   <Layout location={props.location}>
     <SEO
       title='404: Not found'
