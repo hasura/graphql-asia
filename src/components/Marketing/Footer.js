bd5a06c0f7fa493de61280788557f9175c3ceed1
Jigyasu Arya <arya.jigyasu6815@gmail.com>
2021-01-19 14:50:57 +0530

    marketing page: add privacy policy (#58)

--- src/components/Marketing/Footer.js
+++ src/components/Marketing/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import hasuraLogo from '../Common/images/powered-logo-dark-mode.svg';
 import footerPattern from '../Common/images/footer-pattern.svg';
@@ -11,7 +12,32 @@ export const Footer = () => {
       <hr className="light-hr" />
       <hr className="red-hr" />
       <div className="marketing-footer-section flex">
-        <div className="articleDesc">{`© ${currentYear} Hasura Inc. All rights reserved`}</div>
+        <div className="flex flex-column">
+          <ul className="articleDesc flex">
+            <li className="darkModeBorRight" style={{ paddingRight: '15px', marginRight: '15px' }}>
+              <a
+                href="https://hasura.io/legal/website-terms-of-use"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Terms &amp; Conditions
+              </a>
+            </li>
+            <li className="darkModeBorRight" style={{ paddingRight: '15px', marginRight: '15px' }}>
+              <a
+                href="https://hasura.io/legal/hasura-privacy-policy/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Privacy Policy
+              </a>
+            </li>
+            <li className="darkModeBorRight" style={{ border: 'none ' }}>
+              <Link to="/code-of-conduct/">Code Of Conduct</Link>
+            </li>
+          </ul>
+          <div className="articleDesc">{`© ${currentYear} Hasura Inc. All rights reserved`}</div>
+        </div>
         <div className="poweredBrand">
           <a href="https://hasura.io/">
             <img src={hasuraLogo} alt="Powered Logo" />
