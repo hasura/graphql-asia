6d5978b798a092501289f9cc59e8d334156c535c
surendran <surendran@hasura.io>
2021-01-20 12:20:56 +0530

    header link updated

--- src/components/Marketing/Header.js
+++ src/components/Marketing/Header.js
@@ -11,7 +11,9 @@ export const Header = ({ isMobile }) => {
       <div className="marketing-header-section">
         <div className="flex space-between align-center">
           <div className="col-md-3 noPadd">
-            <img src={gqlaImg} alt="graphql-asia" id="gqla-logo" />
+            <Link to='/'>
+              <img src={gqlaImg} alt="graphql-asia" id="gqla-logo" />
+            </Link>
             <div className="articleDesc redColor fontBold">24th &amp; 25th Feb</div>
           </div>
           <div className="poweredBrand col-md-2 flex flex-end noPadd">
@@ -30,16 +32,16 @@ export const Header = ({ isMobile }) => {
   return (
     <div className="marketing-header-section">
       <div className="col-md-3 noPadd">
-        <img src={gqlaImg} alt="graphql-asia" id="gqla-logo" />
+        <Link to='/'>
+          <img src={gqlaImg} alt="graphql-asia" id="gqla-logo" />
+        </Link>
         <div className="articleDesc redColor fontBold">24th &amp; 25th Feb</div>
       </div>
       <div className="mainHeading col-md-7 noPadd pr-20">
         Asia&apos;s largest GraphQL Conf is back
       </div>
       <div className="poweredBrand col-md-2 flex flex-end noPadd">
-        <a href="https://hasura.io/">
-          <img src={hasuraLogo} alt="Powered Logo" />
-        </a>
+        <img src={hasuraLogo} alt="Powered Logo" />
       </div>
     </div>
   );
